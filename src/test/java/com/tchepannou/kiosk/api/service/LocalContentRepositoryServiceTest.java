630772d4cfb9b11b551971f6275dc28ffa5f306f
htchepannou <htchepannou@expedia.com>
2016-09-12 18:36:58 -0400

    improve coverage

---
+++ src/test/java/com/tchepannou/kiosk/api/service/LocalContentRepositoryServiceTest.java
@@ -0,0 +1,13 @@
+package com.tchepannou.kiosk.api.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LocalContentRepositoryServiceTest {
+
+    @Test
+    public void testWrite() throws Exception {
+
+    }
+}
