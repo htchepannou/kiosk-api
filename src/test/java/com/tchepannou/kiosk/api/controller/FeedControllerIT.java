49849eacd1f83e6f373d8e9bb2e917e739248804
htchepannou <htchepannou@expedia.com>
2016-09-13 21:17:52 -0400

    add integration test

--- src/test/java/com/tchepannou/kiosk/api/controller/FeedControllerIT.java
+++ src/test/java/com/tchepannou/kiosk/api/controller/FeedControllerIT.java
@@ -10,13 +10,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.jdbc.Sql;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 import static com.jayway.restassured.RestAssured.when;
 import static org.hamcrest.Matchers.is;
 
-@RunWith(SpringRunner.class)
+@RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Starter.class)
 @WebAppConfiguration
 @IntegrationTest("server.port:0")
