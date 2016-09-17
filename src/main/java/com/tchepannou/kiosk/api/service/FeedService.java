4f2d5ec0658574b64bd402d4bdecbdfa9c5e0405
htchepannou <htchepannou@expedia.com>
2016-09-16 20:02:28 -0400

    add transaction ID to feed list

--- src/main/java/com/tchepannou/kiosk/api/service/FeedService.java
+++ src/main/java/com/tchepannou/kiosk/api/service/FeedService.java
@@ -1,9 +1,9 @@
 package com.tchepannou.kiosk.api.service;
 
 import com.tchepannou.kiosk.api.domain.Feed;
-import com.tchepannou.kiosk.client.dto.FeedListResponse;
 import com.tchepannou.kiosk.api.jpa.FeedRepository;
 import com.tchepannou.kiosk.api.mapper.FeedMapper;
+import com.tchepannou.kiosk.client.dto.FeedListResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class FeedService {
