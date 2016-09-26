5f3481d63f3677198d87a95130d21ce079535f12
htchepannou <htchepannou@expedia.com>
2016-09-25 20:35:26 -0400

    make processing async

--- src/main/java/com/tchepannou/kiosk/api/controller/ArticleController.java
+++ src/main/java/com/tchepannou/kiosk/api/controller/ArticleController.java
@@ -19,6 +19,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -51,6 +52,7 @@ public class ArticleController {
         return toResponseEntity(response);
     }
 
+    @Async
     @ApiOperation("Process the content of all the published articles")
     @RequestMapping(value = "/process", method = RequestMethod.GET)
     public void process() {
