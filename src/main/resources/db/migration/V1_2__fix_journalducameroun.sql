a7dbe35407231e7e30341d5f8197c62cb60fa86e
htchepannou <htchepannou@expedia.com>
2016-10-15 15:09:04 -0400

    improve filtering

--- src/main/resources/db/migration/V1_2__fix_journalducameroun.sql
+++ src/main/resources/db/migration/V1_2__fix_journalducameroun.sql
@@ -1,2 +1,1 @@
-UPDATE website SET article_url_prefix = '/article.php' WHERE id = 1010;
-UPDATE website SET title_sanitize_regex = '.[^:]+:+(.[^:]+)';
+UPDATE website SET article_url_prefix = '/article.php', title_sanitize_regex = '.[^:]+:+(.[^:]+)' WHERE id = 1010;
