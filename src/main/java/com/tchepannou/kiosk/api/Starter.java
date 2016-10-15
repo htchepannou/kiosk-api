ba33d994c6a0619d7b08f5080c23379c4ce68c62
htchepannou <htchepannou@expedia.com>
2016-10-15 10:52:48 -0400

    schedule ranking

--- src/main/java/com/tchepannou/kiosk/api/Starter.java
+++ src/main/java/com/tchepannou/kiosk/api/Starter.java
@@ -2,8 +2,12 @@ package com.tchepannou.kiosk.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
+@EnableAsync
 public class Starter {
     //-- Main
     public static void main(final String[] args) {
