3864e68b55b780a04bdbec498b464dc798b3f240
htchepannou <htchepannou@expedia.com>
2016-10-22 19:01:50 -0400

    fix image selector

--- src/main/resources/db/migration/V1_7__fix_css_selector.sql
+++ src/main/resources/db/migration/V1_7__fix_css_selector.sql
@@ -3,3 +3,7 @@ UPDATE website SET title_sanitize_regex = 'Journal Du Cameroun.com:+(.[^:]+)' WH
 
 -- cameroon-info.net
 UPDATE website SET title_sanitize_regex = '.[^:]+:+(.[^:]+)' WHERE id = 1101;
+
+-- JeWandaMagazine
+UPDATE feed SET url = 'http://www.jewanda-magazine.com/feed/' WHERE id = 1013;
+UPDATE website SET image_css_selector = NULL WHERE id = 1013;
