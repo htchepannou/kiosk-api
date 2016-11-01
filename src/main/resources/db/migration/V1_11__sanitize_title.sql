8025ba19f32df2216c4a9a4524f76ce79a819f85
htchepannou <htchepannou@expedia.com>
2016-11-01 06:49:58 -0400

    filter title

---
+++ src/main/resources/db/migration/V1_11__sanitize_title.sql
@@ -0,0 +1,4 @@
+UPDATE website SET title_sanitize_regex = '.[^:]+:+(.[^:]+)' WHERE id = 1005;
+UPDATE website SET title_sanitize_regex = 'Journal Du Cameroun.com:+(.[^:]+)' WHERE id = 1010;
+UPDATE website SET title_sanitize_regex = '.[^:]+:+(.[^:]+)' WHERE id = 1013;
+UPDATE website SET title_sanitize_regex = '.[^:]+:+(.[^:]+)' WHERE id = 1101;
