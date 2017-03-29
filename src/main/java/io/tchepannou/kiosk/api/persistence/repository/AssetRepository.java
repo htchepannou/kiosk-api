3ee33be56b496c68572a08ce68c21e9b70a6d168
htchepannou <htchepannou@expedia.com>
2017-03-29 17:41:19 -0400

    use links

--- src/main/java/io/tchepannou/kiosk/api/persistence/repository/AssetRepository.java
+++ src/main/java/io/tchepannou/kiosk/api/persistence/repository/AssetRepository.java
@@ -1,14 +1,13 @@
 package io.tchepannou.kiosk.api.persistence.repository;
 
-import io.tchepannou.kiosk.api.persistence.domain.Image;
+import io.tchepannou.kiosk.api.persistence.domain.Asset;
 import io.tchepannou.kiosk.api.persistence.domain.Link;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Collection;
 import java.util.List;
 
 @Repository
-public interface ImageRepository extends CrudRepository<Image, Long> {
-    List<Image> findByLinkIn(Collection<Link> links);
+public interface AssetRepository extends CrudRepository<Link, Long> {
+    List<Asset> findByLinkIn(List<Link> links);
 }
--- src/test/java/io/tchepannou/kiosk/api/service/Fixtures.java
+++ src/test/java/io/tchepannou/kiosk/api/service/Fixtures.java
@@ -1,10 +1,8 @@
 package io.tchepannou.kiosk.api.service;
 
-import io.tchepannou.kiosk.api.persistence.domain.Article;
 import io.tchepannou.kiosk.api.persistence.domain.Feed;
-import io.tchepannou.kiosk.api.persistence.domain.Image;
 import io.tchepannou.kiosk.api.persistence.domain.Link;
-import io.tchepannou.kiosk.api.persistence.domain.Video;
+import io.tchepannou.kiosk.api.persistence.domain.LinkStatusEnum;
 
 import java.util.Date;
 
@@ -20,40 +18,14 @@ public class Fixtures {
         return feed;
     }
 
-    public static Link createLink(final Feed feed) {
-        final Link link = new Link();
-        link.setId(++uid);
-        link.setFeed(feed);
-        link.setUrl("http://ggg.com/" + uid);
-        return link;
-    }
-
-    public static Video createVideo (final Link link){
-        final Video video = new Video();
-        video.setEmbedUrl("http://you.be/embed/1290190");
-        video.setId(++uid);
-        video.setLink(link);
-        return video;
-    }
-
-
-    public static Image createImage(final Link link, final int type) {
-        final Image img = new Image();
-        img.setId(++uid);
-        img.setLink(link);
-        img.setS3Key("dev/content/222/11/" + img.getId() + ".png");
-        img.setType(type);
-        return img;
-    }
-
-    public static Article createArticle(final Link link) {
-        final Article article = new Article();
+    public static Link createArticle(final Feed feed) {
+        final Link article = new Link();
+        article.setFeed(feed);
         article.setId(++uid);
         article.setDisplayTitle("DisplayTitle");
-        article.setLink(link);
         article.setPublishedDate(new Date());
         article.setS3Key("dev/content/222/11/1.html");
-        article.setStatus(0);
+        article.setStatus(LinkStatusEnum.created);
         article.setSummary("Sumary");
         article.setTitle("Title");
         return article;
